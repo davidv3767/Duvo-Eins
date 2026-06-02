c4d71da3cc1f0890666f09c0d4444017a8ec35b2
David Vuddandam <davidvuddandam@gmail.com>
2026-06-02 14:12:03 -0500

    Stardance Devlog 3 Commit

--- duvoeins_main.py
+++ duvoeins_main.py
@@ -18,7 +18,7 @@ def init_game():
     # Returns screen
     return screen
 
-# Initializes and returns all items needed for menus creen
+# Initializes and returns all items needed for menu screen
 def setup_menu_assets():
     # Sets up assets
     assets = {}
@@ -53,44 +53,148 @@ def draw_menu(screen, assets):
     # Update display
     pygame.display.flip()
 
-# Processes player inputs & contains game loop
-def handle_events(assets):
+# Processes player inputs at menu screen
+def handle_menu_events(assets):
     for event in pygame.event.get():
         # Quits game
         if event.type == pygame.QUIT:
-            return False
-        # Also guits game
-        elif event.type == pygame.KEYDOWN:
+            return "quit"
+        elif event.type == pygame.KEYDOWN:  
+            # Also quits game
             if event.key == pygame.K_ESCAPE:
-                return False
+                return "quit"
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left click
                 mouse_pos = event.pos
                 # Buttons on menu screen
                 if assets['start_button_rect'].collidepoint(mouse_pos):
-                    print("THIS WORKS")
+                    return "selection1"
                 elif assets['info_button_rect'].collidepoint(mouse_pos):
-                    print("THIS ALSO WORKS")
-                    
-    return True
+                    return "info"
+    # If nothing is returned, stay on menu page               
+    return "menu"
+
+# Initializes and returns all items needed for menu screen
+def setup_info_assets():
+    # Sets up assets
+    assets = {}
+    # Title Setup
+    title_font = pygame.font.SysFont("showcardgothic", 150)
+    assets['title_surface'] = title_font.render("Information", True, (255, 255, 255))
+    assets['title_rect'] = assets['title_surface'].get_rect(center=(WIDTH / 2, HEIGHT / 4))
+    # Version & Credits Setup
+    version_font = pygame.font.SysFont(None, 75)
+    assets['version_surface'] = version_font.render("Version: 0.1.1", True, (255, 255, 255))
+    assets['version_rect'] = assets['version_surface'].get_rect(center=(WIDTH / 4, HEIGHT / 2))
+    maindev_font = pygame.font.SysFont(None, 75)
+    assets['maindev_surface'] = maindev_font.render("Made by David Vuddandam", True, (255, 255, 255))
+    assets['maindev_rect'] = assets['maindev_surface'].get_rect(center=(WIDTH / 4, HEIGHT / 8 * 5))
+    # Instructions Setup
+    instrtitle_font = pygame.font.SysFont(None, 125)
+    assets['instrtitle_surface'] = instrtitle_font.render("Instructions", True, (255, 255, 255))
+    assets['instrtitle_rect'] = assets['instrtitle_surface'].get_rect(center=(WIDTH / 4 * 3, HEIGHT / 7 * 3))
+    instructions_font = pygame.font.SysFont(None, 50)
+    # Returns assets
+    return assets
+
+# Handles visuals for info screen
+def draw_info(screen, assets):
+    screen.fill((40, 40, 40))
+    # Draws Title
+    screen.blit(assets['title_surface'], assets['title_rect'])
+    # Draws Version & Credits Info
+    screen.blit(assets['version_surface'], assets['version_rect'])
+    screen.blit(assets['maindev_surface'], assets['maindev_rect'])
+    # Draws Instructions
+    screen.blit(assets['instrtitle_surface'], assets['instrtitle_rect'])
+    pygame.display.flip()
+
+# Processes player inputs at info screen
+def handle_info_events():
+    for event in pygame.event.get():
+        # Quits game
+        if event.type == pygame.QUIT:
+            return "quit"
+        elif event.type == pygame.KEYDOWN:
+            # Also quits game
+            if event.key == pygame.K_ESCAPE:
+                return "quit"
+            # Returns to menu page
+            elif event.key == pygame.K_TAB:
+                return "menu"
+    # If nothing is returned, stay on info page               
+    return "info"
+
+# Initializes and returns all items needed for first selection screen
+def setup_selection1_assets():
+    # Set up assets
+    assets = {}
+    # Set up player name graphics
+    playername_font = pygame.font.SysFont(None, 120)
+    assets['p1name_surface'] = playername_font.render("P1 Name: ", True, (255, 255, 255))
+    assets['p1name_rect'] = assets['p1name_surface'].get_rect(center=(WIDTH / 2, HEIGHT / 8 * 3))
+    assets['p2name_surface'] = playername_font.render("P2 Name: ", True, (255, 255, 255))
+    assets['p2name_rect'] = assets['p2name_surface'].get_rect(center=(WIDTH / 2, HEIGHT / 8 * 5))
+    # Returns assets
+    return assets
+
+# Handles visuals for first selection screen
+def draw_selection1(screen, assets):
+    screen.fill((40, 40, 40))
+    # Draws player name graphics
+    screen.blit(assets['p1name_surface'], assets['p1name_rect'])
+    screen.blit(assets['p2name_surface'], assets['p2name_rect'])
+    pygame.display.flip()
+
+# Processes player inputs at info screen
+def handle_selection1_events(active_box):
+    for event in pygame.event.get():
+        # Quits game
+        if event.type == pygame.QUIT:
+            return "quit"
+        elif event.type == pygame.KEYDOWN:
+            # Also quits game
+            if event.key == pygame.K_ESCAPE:
+                return "quit"
+            # Returns to menu page
+            elif event.key == pygame.K_TAB:
+                # Going back to menu page/p1 box
+                print("tab s1")
+            elif event.key == pygame.K_BACKSPACE:
+                # Delete last character
+                print("back s1")
+            else:
+                if event.unicode.isprintable() and len(event.unicode) > 0:
+                    print("add s1")
+    # If nothing is returned, stay on first selection page               
+    return "selection1"
 
 # Main execution function
 def main():
-    # Runs other functions
+    # Runs setup functions
     screen = init_game()
-    assets = setup_menu_assets()
+    menu_assets = setup_menu_assets()
+    info_assets = setup_info_assets()
+    selection1_assets = setup_selection1_assets()
     clock = pygame.time.Clock()
-    
+    # Current state
     current_state = "menu"
-    running = True
-    
-    while running:
+    # Player names
+    p1_name = ""
+    p2_name = ""
+    active_box = "p1"
+    # Main game loop
+    while current_state != "quit":
         if current_state == "menu":
-            draw_menu(screen, assets)
-            running = handle_events(assets)
-        
+            draw_menu(screen, menu_assets)
+            current_state = handle_menu_events(menu_assets)
+        elif current_state == "info":
+            draw_info(screen, info_assets)
+            current_state = handle_info_events()
+        elif current_state == "selection1":
+            draw_selection1(screen, selection1_assets)
+            current_state = handle_selection1_events(active_box)
         clock.tick(60)
-
     # End mechanic
     pygame.quit()
     sys.exit()
